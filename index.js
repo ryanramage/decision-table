f7da4c377e140b3ee6633d45925ecb3031f49c76
Ryan Ramage <ryan.ramage@gmail.com>
2015-04-23 09:10:33 -0600

    use standard style. consolidate to run method

--- index.js
+++ index.js
@@ -1,56 +1,47 @@
-module.exports = DecisionTable;
+module.exports = DecisionTable
 
-var levelup = require('levelup');
-var selectn = require('selectn');
+var levelup = require('levelup')
+var selectn = require('selectn')
 
-function DecisionTable(columns, table, opts){
-	if (!opts) opts = {};
-	var db_path = opts.db_path || '/' + 'decisiontable/' + Date.now();
+function DecisionTable (columns, table, opts) {
+  if (!opts) opts = {}
+  var db_path = opts.db_path || '/' + 'decisiontable/' + Date.now()
 
-	var self = this;
+  var self = this
 
-	this.columns = columns;
-	this.db = levelup(db_path, { 
-		db: opts.db_type || require('memdown'),
-		keyEncoding: 'json',
-		valueEncoding: 'json'
-	})
+  this.columns = columns
+  this.db = levelup(db_path, {
+    db: opts.db_type || require('memdown'),
+    keyEncoding: 'json',
+    valueEncoding: 'json'
+  })
 
-	if (!table || typeof table !== 'object' || table.length === 0) return;
-	table.forEach(function(row){
-		if (!row || typeof row !== 'object' || row.length === 0) return;
+  if (!table || typeof table !== 'object' || table.length === 0) return
+  table.forEach(function (row) {
+    if (!row || typeof row !== 'object' || row.length === 0) return
 
-		var val = row.pop();
-		self.db.put(row, val)
-	})
+    var val = row.pop()
+    self.db.put(row, val)
+  })
 }
 
-DecisionTable.prototype.addRow = function(row) {
-	if (!row || typeof row !== 'object' || row.length === 0) return;
-	var val = row.pop();
-	this.db.put(row, val)
-};
+DecisionTable.prototype.addRow = function (row) {
+  if (!row || typeof row !== 'object' || row.length === 0) return
+  var val = row.pop()
+  this.db.put(row, val)
+}
 
+DecisionTable.prototype.run = function (thing, cb) {
+  var key = []
 
+  if (Array.isArray(thing)) {
+    key = thing
+  } else if (typeof thing === 'object') {
+    this.columns.forEach(function (col) {
+      var col_entry = selectn(col, thing)
+      key.push(col_entry)
+    })
+  }
+  this.db.get(key, cb)
 
-DecisionTable.prototype.obj = function(obj, cb) {
-	var key = [];
-
-	this.columns.forEach(function(col){
-		var col_entry = selectn(col, obj);
-		key.push(col_entry);
-	})
-
-	this.db.get(key, cb);
-
-};
-
-DecisionTable.prototype.arr = function(array, cb) {
-	var key = [];
-	array.forEach(function(col_entry){
-		key.push(col_entry)
-	});
-	this.db.get(key, cb);
-
-};
-
+}
